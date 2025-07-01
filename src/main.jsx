6a494cc90d97e2486c34b5ddd994386a5a500d42
Thisal Tennakoon <thisaltennakoon@gmail.com>
2025-07-01 11:06:43 +0530

    Change font

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,7 @@ import MCPInspector from './app/MCPInspector.tsx'
 
 ReactDOM.render(
     <React.StrictMode>
-        <MCPInspector
-            shouldSetHeaderNameExternally={true}
-        />
+        <MCPInspector/>
     </React.StrictMode>,
     document.getElementById('root')
 )
